38b8c27199ab0ec22e165b59cf639e098f79606e
wangan0551 <wangan0551@163.com>
2017-01-08 13:22:23 +0800

    change https

--- js/base.js
+++ js/base.js
@@ -1,7 +1,7 @@
 
-document.writeln('<link href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">');
-document.writeln('<script src="http://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>')
-document.writeln('<script src="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>')
+document.writeln('<link href="//cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">');
+document.writeln('<script src="https//apps/bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>')
+document.writeln('<script src="//cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>')
 /*	<!-- 新 Bootstrap 核心 CSS 文件 -->	
 	<link href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 	<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
