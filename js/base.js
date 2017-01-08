204e57dac6ed6bc2442e7028f4c9b733a78fda98
wangan0551 <wangan0551@163.com>
2017-01-08 12:57:15 +0800

    first building

---
+++ js/base.js
@@ -0,0 +1,12 @@
+
+document.writeln('<link href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">');
+document.writeln('<script src="http://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>')
+document.writeln('<script src="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>')
+/*	<!-- 新 Bootstrap 核心 CSS 文件 -->	
+	<link href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+	<script src="http://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
+ 
+	<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+	<script src="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+*/
---
+++ js/jumbotron.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  UpdJumbotron();
+});
+function UpdJumbotron(){
+	$('.jumbotron').empty();
+	$('.jumbotron').prepend("<h1>wdw</h1>");// 标题
+	$('.jumbotron').append("<p>this is JQuery done!</p>"); //简介
+	$('.jumbotron').append("<a class='btn btn-primary btn-large' href='test/MyForm.html'>Learn more</a>");//详细信息按钮。
+}
+
+function UpdTechnology(){
+	
+}
+
+function UpdTools(){
+	
+	
+}
+function UpdImg(){
+	
+	
+}
