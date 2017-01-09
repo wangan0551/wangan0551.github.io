e0c0c8afff67a7746c236d6b6ec262a7f41d9de8
wangan0551 <wangan0551@163.com>
2017-01-09 20:52:32 +0800

    add jsq function

--- js/jumbotron.js
+++ js/jumbotron.js
@@ -1,6 +1,33 @@
 $(document).ready(function(){
   UpdJumbotron();
+  JSQ();
 });
+function JSQ(){
+  
+   $('#tools button').click(function(){
+	   var gs = $('#jsqText').val();
+	   if (gs.length <= 0){
+		   alert('请输入计算公式！');
+	   }else{
+		   var Xseach = /[^0-9,+,-,*,\/,(,),\.,%]/g.exec(gs);
+		   if ( Xseach!=null)
+			{
+				alert("输入的公式有误:"+Xseach);
+			}else {
+				try{
+					var res = eval(gs);
+				}catch(err){
+					alert("输入的公式有误，请检查！\n"+err.message);
+				}
+				alert(res);
+				$('#result').text("result = "+res.toString());
+				
+		}
+	   }
+	
+});
+
+}
 function UpdJumbotron(){
 	$('.jumbotron').empty();
 	$('.jumbotron').prepend("<h1>wdw</h1>");// 标题
