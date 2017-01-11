12e441c476adee207b085c21c1f083eeae453b15
wangan0551 <wangan0551@163.com>
2017-01-11 10:53:10 +0800

    update jquery.main.js

--- js/base.js
+++ js/base.js
@@ -1,5 +1,5 @@
 ﻿document.writeln('<link href="//cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">');
-document.writeln('<script src="https//apps/bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>')
+document.writeln('<script src="//apps/bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>')
 document.writeln('<script src="//cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>')
 /*	<!-- 新 Bootstrap 核心 CSS 文件 -->	
 	<link href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
