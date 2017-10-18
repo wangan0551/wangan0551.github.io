47d6dc2dc928746509829feddee1f31bd764a2b0
wangan0551 <wangan0551@163.com>
2017-10-18 11:18:36 +0800

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,11 +1,11 @@
 document.writeln('<link href="//cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">');
 document.writeln('<script src="//apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>')
 document.writeln('<script src="//cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>')
-/*	<!-- 新 Bootstrap 核心 CSS 文件 -->	
+	<!-- 新 Bootstrap 核心 CSS 文件 -->	
 	<link href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 	<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
 	<script src="http://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
  
 	<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
 	<script src="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-*/
+
